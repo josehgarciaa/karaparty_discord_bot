e121b55991d818933e3e80e5fd72b109cf3f1dda
Jose Garcia <you@example.com>
2025-04-29 13:05:17 +0200

    improve the template for configuration

--- kai_request.py
+++ kai_request.py
@@ -1,6 +1,24 @@
 import requests
 
 # URL of your FastAPI server
+import os
+import yaml
+from utils.error_reporter import report_error
+
+# Path to the YAML file
+config_path = "configs/config.yaml"
+try:
+    print(config_path)
+    with open(config_path, "r", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+except Exception as e:
+    report_error(e, context="Please, remember to fill your configs/config.yaml file. Template is at config.yaml.template")
+    raise e  # Stop startup if config cannot be loadedp startup if config cannot be loaded
+
+url = config["kai_api"]["song_endpoint"]
+
+print(url)
+
 
 try:
     response = requests.get(url)
